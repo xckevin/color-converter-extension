ddfe183d4072d83b73b67ec4577296ccf8f8b4fa
kevin.liu <xckevin927@gmail.com>
2024-10-11 19:27:45 +0800

    declare manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,10 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  manifest: {
+    version: '1.0.0',
+    name: 'color-converter',
+    description: 'A color converter tool',    
+    author: 'kai',
+  }
 });
