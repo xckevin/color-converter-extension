988e128fd1bf1c792b4fe626504cdc599e4f97ed
kevin.liu <xckevin927@gmail.com>
2024-10-11 11:46:13 +0800

    fix content js

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,6 @@
-// export default defineContentScript({
-//   matches: ['*://*.google.com/*'],
-//   main() {
-//     console.log('Hello content.');
-//   },
-// });
+export default defineContentScript({
+  matches: ['*://*'],
+  main() {
+    
+  },
+});
