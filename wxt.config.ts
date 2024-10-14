ae2e0e9640e563870945044a7db6921535616593
kevin.liu <xckevin927@gmail.com>
2024-10-14 23:52:02 +0800

    remove unneccassary files and upgrade version

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ["@wxt-dev/module-react"],
   manifest: {
-    version: '1.0.0',
-    name: 'color-converter',
-    description: 'A color converter tool',    
-    author: 'kai',
-  }
+    version: "1.0.1",
+    name: "color-converter",
+    description: "A color converter tool",
+    author: "kai",
+  },
 });
