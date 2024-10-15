ef1949e0f9d76a85889097f5f434f87180f4ffdf
kevin.liu <xckevin927@gmail.com>
2024-10-15 10:37:32 +0800

    change output dir

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
+  outDir: "dist",
   manifest: {
     version: "1.0.1",
     name: "color-converter",
